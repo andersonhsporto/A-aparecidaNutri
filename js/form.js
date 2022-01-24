748dbe8602d814427d277ad54ef2789a4f8fc8fa
anhigo-s <anderson.higo2@gmail.com>
2022-01-24 00:02:17 -0300

    fix: form.js

--- js/form.js
+++ js/form.js
@@ -3,32 +3,48 @@ botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
 
     var form = document.querySelector("#form-add");
+	var paciente = criarPaciente(form);
+    var pacienteTr = criarTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
 
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+    tabela.appendChild(pacienteTr);
+});
 
-    var pacienteTr = document.createElement("tr");
+function criarPaciente(form){
 
-    var nomeTd = document.createElement("td");
+	var paciente = {
+		nome: form.nome.value,
+		peso: form.peso.value,
+		altura: form.altura.value,
+		gordura: form.gordura.value,
+		imc: calculaImc(form.peso.value, form.altura.value)
+	}
+	return (paciente);
+}
+
+function criarTr(paciente){
+	var pacienteTr = document.createElement("tr");
+
+	var nomeTd = document.createElement("td");
     var pesoTd = document.createElement("td");
     var alturaTd = document.createElement("td");
     var gorduraTd = document.createElement("td");
     var imcTd = document.createElement("td");
+	var indexTd = document.createElement("td");
 
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
+    nomeTd.textContent = paciente.nome;
+    pesoTd.textContent = paciente.peso;
+    alturaTd.textContent = paciente.altura;
+    gorduraTd.textContent = paciente.gordura;
+	imcTd.textContent = paciente.imc;
+	indexTd.textContent = classificaImc(paciente.imc);
 
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
+	pacienteTr.appendChild(imcTd);
+	pacienteTr.appendChild(indexTd);
 
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-
-});
+	return (pacienteTr);
+}
